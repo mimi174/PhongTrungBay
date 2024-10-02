15ae8188d5298090ea94fadc533bd70f259ad717
sarangmiemie@gmail.com <sarangmiemie@gmail.com>
2024-10-03 03:22:33 +0700

    cmit to window

---
+++ Assets/Scripts/ClickHandle.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class ClickHandle: MonoBehaviour
+{
+
+    [SerializeField] private Camera mainCamera;
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (IsPointerOverUIObject()) return;
+
+            var hit = PerformRaycast();
+            if (hit.collider && IsInteractableTag(hit.collider.tag)) {
+
+            }
+
+        }
+    }
+
+    private bool IsInteractableTag(string tag)
+    {
+        return tag == "";
+    }
+
+    private RaycastHit PerformRaycast()
+    {
+        var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out var hit, Mathf.Infinity);
+        return hit;
+    }
+
+
+
+    public static bool IsPointerOverUIObject()
+    {
+        var results = new List<RaycastResult>();
+
+        var eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+
+        return results.Count > 0;
+    }
+}
